02ccf8b71dca2320c1768e1e60f3993091f1059f
LJunJoy <904312309@qq.com>
2019-01-12 10:47:30 +0800

    '增加n快捷键'

--- src/first.js
+++ src/first.js
@@ -6,10 +6,22 @@ Person.prototype.sayName = function () {
     console.log(this.name);
 }
 
-var person1 = new Person();
-person1.sayName(); // 'Zaxlct'
+function print16(chicun){
+    var c=chicun*2.54;
+    var cb=Math.sqrt(16*16+9*9);
+    console.log(c);
+    var a=c*16/cb;
+    var b=c*9/cb;
+    console.log(a+":"+b);
 
-var person2 = new Person();
-person2.sayName(); // 'Zaxlct'
+    console.log();
+}
 
-console.log(person1.sayName == person2.sayName); //true
+var scope="global";
+function checkScope(){
+    console.log(scope);
+    var scope="local";
+    console.log(scope);
+}
+checkScope();
+
